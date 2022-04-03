5270d7c435bcf9f3d342cb4593198b098137215f
rahimuj570 <rahimuj570@gmail.com>
2022-04-04 00:38:36 +0600

    nav bar added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Blog from "./Components/Blog/Blog";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Header from "./Components/Header/Header";
+import Home from "./Components/Home/Home";
+import Reviews from "./Components/Reviews/Reviews";
+import About from "./Components/About/About";
+import NotFound404 from "./Components/NotFound404/NotFound404";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/reviews" element={<Reviews />}></Route>
+        <Route path="/dashboard" element={<Dashboard />}></Route>
+        <Route path="/blog" element={<Blog />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound404 />}></Route>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="text-center px-1 mt-10">
+      <NavLink
+        className={({ isActive }) =>
+          (isActive
+            ? "shadow px-1 border border-b-8 border-b-indigo-300 rounded bg-sky-50"
+            : "") + " text-md sm:text-xl mr-3 shadow px-1  py-1"
+        }
+        to={"/"}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          (isActive
+            ? "shadow px-1 border border-b-8 border-b-indigo-300 rounded bg-sky-50"
+            : "") + " text-md sm:text-xl mr-3 shadow px-1 py-1"
+        }
+        to={"/reviews"}
+      >
+        Reviews
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          (isActive
+            ? "shadow px-1 border border-b-8 border-b-indigo-300 rounded bg-sky-50"
+            : "") + " text-md sm:text-xl mr-3 shadow px-1 py-1"
+        }
+        to={"/dashboard"}
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          (isActive
+            ? "shadow px-1 border border-b-8 border-b-indigo-300 rounded bg-sky-50"
+            : "") + " text-md sm:text-xl mr-3 shadow px-1 py-1"
+        }
+        to={"/blog"}
+      >
+        Blog
+      </NavLink>
+      <NavLink
+        className={({ isActive }) =>
+          (isActive
+            ? "shadow px-1 border border-b-8 border-b-indigo-300 rounded bg-sky-50"
+            : "") + " text-md sm:text-xl mr-3 shadow px-1 py-1"
+        }
+        to={"/about"}
+      >
+        About
+      </NavLink>
+    </div>
+  );
+};
+
+export default Header;
