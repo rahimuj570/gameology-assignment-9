642c730cb66f749c01902279f85a033384d68e10
rahimuj570 <rahimuj570@gmail.com>
2022-04-04 21:26:04 +0600

    dashboard chart added

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,7 +1,90 @@
 import React from "react";
-
+import {
+  Area,
+  AreaChart,
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ComposedChart,
+  Legend,
+  Line,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import ChartData from "../FakeData/ChartData.json";
 const Dashboard = () => {
-  return <div></div>;
+  return (
+    <div className="sm:my-20 my-10 mx-5 grid grid-cols-1 lg:grid-cols-2 justify-center">
+      <div className="w-full">
+        <ResponsiveContainer width="100%" height={400}>
+          <AreaChart
+            data={ChartData}
+            margin={{ top: 10, right: 30, left: 10, bottom: 0 }}
+          >
+            <defs>
+              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey={"month"} />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Tooltip />
+            <Legend />
+            <Area
+              type="monotone"
+              dataKey="sell"
+              stroke="#8884d8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="investment"
+              stroke="#82ca9d"
+              fillOpacity={1}
+              fill="url(#colorPv)"
+            />
+            <Area
+              type="monotone"
+              dataKey="revenue"
+              stroke="#818CF8"
+              fillOpacity={1}
+              fill="url(#colorUv)"
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+      <div className="w-full">
+        <ResponsiveContainer data={ChartData} width="95%" height={400}>
+          <ComposedChart data={ChartData}>
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid stroke="#f5f5f5" />
+            <Area
+              type="monotone"
+              dataKey="investment"
+              fill="#8884d8"
+              stroke="#8884d8"
+            />
+            <Bar dataKey="revenue" barSize={20} fill="#413ea0" />
+            <Line type="monotone" dataKey="revenue" stroke="#ff7300" />
+          </ComposedChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
