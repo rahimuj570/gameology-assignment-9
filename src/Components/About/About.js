7a1dca9662574add8826abcb2c9aa4baa7cd4051
rahimuj570 <rahimuj570@gmail.com>
2022-04-05 22:24:52 +0600

    read.me file added and about page is ready

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div className="m-20 ">
+      <h1 className=" text-center text-3xl font-gos">
+        This is my 9th Assignment Project. This assignment focused on React
+        Router.
+      </h1>
+      <sub className="mt-3 sm:mr-10 float-right font-gos">_rahim</sub>
+    </div>
+  );
 };
 
 export default About;
